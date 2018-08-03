ce660cb40c586751cdbeb2abf4b4a5b409ba0fb0
ag-ayush <ag.goelayush@gmail.com>
2018-08-03 04:37:18 +0000

    added APIKey support and leave ride function

--- rideboard_api/__init__.py
+++ rideboard_api/__init__.py
@@ -1,15 +1,17 @@
-####################################
-# File name: __init__.py           #
-# Author: Ayush Goel               #
-####################################
-import json
+#########################################
+# File name: __init__.py                #
+# Author: Ayush Goel                    #
+#                                       #
+# Credits to Devin for providing        #
+# functions for generating and checking #
+# API keys.                             #
+#########################################
 import os
 import markdown
-from flask import Flask, request, jsonify, session, redirect, Markup
+from flask import Flask, request, jsonify, redirect
 from flask_cors import cross_origin
 from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import UniqueConstraint
 
 # Setting up Flask
 app = Flask(__name__)
@@ -27,7 +29,8 @@ auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"],
                           client_registration_info=app.config["OIDC_CLIENT_CONFIG"])
 
 # pylint: disable=wrong-import-position
-from rideboard_api.models import Ride, Rider, Car
+from rideboard_api.models import Ride, Rider, Car, APIKey
+from .utils import user_auth
 
 
 @app.route('/', methods=['GET'])
@@ -37,68 +40,149 @@ def index():
     return markdown.markdown(f.read(), extensions=['markdown.extensions.tables', 'markdown.extensions.fenced_code'])
 
 
-@app.route('/all', methods=['GET'])
+@app.route('/<api_key>/all', methods=['GET'])
 @cross_origin(headers=['Content-Type'])
-def all_events():
+def all_events(api_key: str):
     """
     Returns all Events in the database
+    :param api_key: API key allowing for the use of the API
     :return: Returns JSON of all quotes in the rideboard database
     """
-    rideid = request.args.get('id')
-    query = []
-    if rideid is not None:
-        # adds a Ride object to the List:query
-        query.append(Ride.query.get(rideid))
-    else:
-        # Makes query a List of all rides
-        query = Ride.query.all()
-    return parse_event_as_json(query)
+    if check_key(api_key):
+        rideid = request.args.get('id')
+        query = []
+        if rideid is not None:
+            # adds a Ride object to the List:query
+            query.append(Ride.query.get(rideid))
+        else:
+            # Makes query a List of all rides
+            query = Ride.query.all()
+        return parse_event_as_json(query)
+    return "Invalid API Key!", 403
 
 
-@app.route('/upcoming', methods=['GET'])
+@app.route('/<api_key>/upcoming', methods=['GET'])
 @cross_origin(headers=['Content-Type'])
-def upcoming_event():
+def upcoming_event(api_key: str):
     """
     Returns the Event with the earliest start_time as JSON
+    :param api_key: API key allowing for the use of the API
     :return: JSON of the upcoming event
     """
-    query = Ride.query.order_by(Ride.start_time.asc()).first()
-    return jsonify(return_event_json(query))
+    if check_key(api_key):
+        query = Ride.query.order_by(Ride.start_time.asc()).first()
+        return jsonify(return_event_json(query))
+    return "Invalid API Key!", 403
 
 
-@app.route('/join/<car_id>/<username>/<first_name>/<last_name>/', methods=['GET'])
+@app.route('/<api_key>/join/<car_id>/<username>/<first_name>/<last_name>/', methods=['GET'])
 @cross_origin(headers=['Content-Type'])
-def join_car(car_id, username: str, first_name: str, last_name: str):
+def join_car(car_id, username: str, first_name: str, last_name: str, api_key: str):
     """
     Adds username to the Car and returns the Event as JSON
     :param car_id: ID of the car to add the username to
     :param username: username
     :param first_name: First name of the user [from CSH LDAP given username]
     :param last_name: Last name of the user [from CSH LDAP given username]
+    :param api_key: API key allowing for the use of the API
     :return: Event as JSON in which the Car belongs or 400 in case of error.
     """
     # TODO: This method only works with GET, but I believe this should be a POST.
-    incar = False
-    name = first_name+" "+last_name
-    car = Car.query.filter(Car.id == car_id).first()
-    event = Ride.query.filter(Ride.id == car.ride_id).first()
-    for c in event.cars:
-        if c.username == username:
-            incar = True
-        for person in c.riders:
-            if person.username == username:
+    if check_key(api_key):
+        incar = False
+        name = first_name+" "+last_name
+        car = Car.query.filter(Car.id == car_id).first()
+        event = Ride.query.filter(Ride.id == car.ride_id).first()
+        for c in event.cars:
+            if c.username == username:
                 incar = True
-    if (car.current_capacity < car.max_capacity or car.max_capacity == 0) and not incar:
-        rider = Rider(username, name, car_id)
-        car.current_capacity += 1
-        db.session.add(rider)
-        db.session.add(car)
+            for person in c.riders:
+                if person.username == username:
+                    incar = True
+        if (car.current_capacity < car.max_capacity or car.max_capacity == 0) and not incar:
+            rider = Rider(username, name, car_id)
+            car.current_capacity += 1
+            db.session.add(rider)
+            db.session.add(car)
+            db.session.commit()
+            return jsonify(return_event_json(event))
+        return "The car is either full, or you have already joined a ride, or you are the owner of one!", 400
+    return "Invalid API Key!", 403
+
+
+@app.route('/<api_key>/leave/<car_id>/<username>/', methods=['GET'])
+@cross_origin(headers=['Content-Type'])
+def leave_ride(car_id, username: str, api_key: str):
+    """
+    Removes username from the Car and returns the associated Event as JSON
+    :param car_id: ID of the car to remove the username from
+    :param username: username
+    :param api_key: API key allowing for the use of the API
+    :return: Event as JSON in which the Car belongs to.
+    """
+    # TODO: Should be a POST; may be use /leave/event_id/username instead or allow both using params instead of route.
+    if check_key(api_key):
+        car = Car.query.filter(Car.id == car_id).first()
+        rider = Rider.query.filter(Rider.username == username, Rider.car_id == car_id).first()
+        event = Ride.query.filter(Ride.id == car.ride_id).first()
+        if rider is not None:
+            db.session.delete(rider)
+            car.current_capacity -= 1
+            db.session.add(car)
+            db.session.commit()
+            return jsonify(return_event_json(event))
+        return "You do not exist in that car!", 400
+    return "Invalid API Key!", 403
+
+
+@app.route('/generatekey/<reason>')
+@auth.oidc_auth
+@user_auth
+def generate_api_key(reason: str, metadata=None):
+    """
+    Creates an API key for the user requested.
+    Using a reason and the username grabbed through the @auth.oidc_auth call
+    :param reason: Reason for the API key
+    :param metadata: auth dictionary
+    :return: Hash of the Key or a String stating an error
+    """
+    if not check_key_unique(metadata['uid'], reason):
+        # Creates the new API key
+        new_key = APIKey(metadata['uid'], reason)
+        # Adds, flushes and commits the new object to the database
+        db.session.add(new_key)
+        db.session.flush()
         db.session.commit()
-        return jsonify(return_event_json(event))
-    return "The car is either full, or you have already joined a ride, or you are the owner of one!", 400
+        return new_key.hash
+    return "There's already a key with this reason for this user!"
 
 
-# TODO: rideform, carform, delete ride, delete car, leave ride, api-key
+# TODO: rideform, carform, delete ride, delete car
+
+
+def check_key(api_key: str) -> bool:
+    """
+    Checks if the key exists.
+    :param api_key: API key
+    :return: true if the key exists in the databse
+    """
+    keys = APIKey.query.filter_by(hash=api_key).all()
+    if keys:
+        return True
+    return False
+
+
+def check_key_unique(owner: str, reason: str) -> bool:
+    """
+    Checks if there is an unique key of a given user and reason
+    :param owner: generator of the key
+    :param reason: reason provided for the key
+    :return: true if the key exists uniquely
+    """
+    keys = APIKey.query.filter_by(owner=owner, reason=reason).all()
+    if keys:
+        return True
+    return False
 
 
 def return_event_json(event: Ride):
